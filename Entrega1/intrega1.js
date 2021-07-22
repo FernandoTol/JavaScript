68114ab6fd8895821d57183f76d7892503a72d7b
FernandoTol <60888678+FernandoTol@users.noreply.github.com>
2021-07-22 13:38:27 -0500

    entrega1

---
+++ Entrega1/intrega1.js
@@ -0,0 +1,8 @@
+let nombre = prompt('Cual es tu nombre?')
+let numero1 = prompt('ingrese un numero');
+let numero2 = prompt('ingrese otro numero para sumar');
+
+
+let resultado = (parseInt(numero1) + parseInt(numero2));
+
+alert(`Hola! ${nombre} tu resultado de la suma es: ` + resultado);
