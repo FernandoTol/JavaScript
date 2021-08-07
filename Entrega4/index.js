d2cc7d91dfd0a454927a8612631bf6619ea24611
FernandoTol <60888678+FernandoTol@users.noreply.github.com>
2021-08-07 09:57:38 -0500

    nuevo intento

--- Entrega4/index.js
+++ Entrega4/index.js
@@ -1,31 +1,26 @@
-//let numero1 = prompt('Primer digito')
-//let numero2 = prompt('sugundo digito')
-//let operacion = prompt('Que operacion va a realizar')
-let cantidadOperaciones = prompt('cantidad')
 
-function suma() {
-    return parseInt(numero1) + parseInt(numero2)
-}
-function resta() {
-    return parseInt(numero1) - parseInt(numero2)
-}
-function dividir() {
-    return parseInt(numero1) / parseInt(numero2)
-}
-function multiplicar() {
-    return parseInt(numero1) * parseInt(numero2)
-}
 
-for(i=0; i<=cantidadOperaciones; i++){let numero1 = prompt('Primer digito')
-let numero2 = prompt('sugundo digito')
-let operacion = prompt('Que operacion va a realizar')
-if (operacion == 'suma' || operacion == '+') {
-    alert(suma())
-}else if (operacion == 'resta' || operacion == '-'){
-    alert(resta());
-}else if (operacion == 'dividir' || operacion == 'divicion'|| operacion == '/'){
-    alert(dividir());
-}else if (operacion == 'multiplicar' || operacion == '*'){
-    alert(multiplicar());
-}
+
+while (true) {
+    let continuacion = prompt('Desea continuar si/no')
+    let numero1 = prompt('Primer digito')
+    let numero2 = prompt('sugundo digito')
+    let operacion = prompt('Que operacion va a realizar')
+
+    const suma = () => parseInt(numero1) + parseInt(numero2);
+    const resta = () => parseInt(numero1) - parseInt(numero2);
+    const dividir = () => parseInt(numero1) / parseInt(numero2);
+    const multiplicar = () => parseInt(numero1) * parseInt(numero2);
+
+    if(continuacion == 'no'){
+        break;
+    }else if (operacion == 'suma' || operacion == '+') {
+        alert(suma());
+    } else if (operacion == 'resta' || operacion == '-') {
+        alert(resta());
+    } else if (operacion == 'dividir' || operacion == 'divicion' || operacion == '/') {
+        alert(dividir());
+    } else if (operacion == 'multiplicar' || operacion == '*') {
+        alert(multiplicar());
+    }
 }
