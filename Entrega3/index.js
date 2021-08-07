22b83dff2df18eebcd97f9f5087625dfe98259e0
FernandoTol <60888678+FernandoTol@users.noreply.github.com>
2021-08-07 10:00:07 -0500

    correcion

--- Entrega3/index.js
+++ Entrega3/index.js
@@ -1,6 +1,4 @@
-let numero1 = prompt('Primer digito')
-let numero2 = prompt('sugundo digito')
-let operacion = prompt('Que operacion va a realizar')
+let cantidadOperaciones = prompt('cantidad')
 
 
 const suma = () => parseInt(numero1) + parseInt(numero2);
@@ -9,13 +7,18 @@ const dividir = () => parseInt(numero1) / parseInt(numero2);
 const multiplicar = () => parseInt(numero1) * parseInt(numero2);
 
 
-if (operacion == 'suma' || operacion == '+' || operacion == 'SUMA' || operacion == 'Suma') {
-    alert(suma())
-} else if (operacion == 'resta' || operacion == '-' || operacion == 'RESTA' || operacion == 'Resta') {
-    alert(resta());
-} else if (operacion == 'dividir' || operacion == 'divicion' || operacion == '/' || operacion == 'Divicion' || operacion == 'DIVICION') {
-    alert(dividir());
-} else if (operacion == 'multiplicar' || operacion == '*' || operacion == 'multiplica' || operacion == 'Multiplicacion') {
-    alert(multiplicar());
+for (i = 0; i <= cantidadOperaciones; i++) {
+    let numero1 = prompt('Primer digito')
+    let numero2 = prompt('sugundo digito')
+    let operacion = prompt('Que operacion va a realizar')
+    if (operacion == 'suma' || operacion == '+') {
+        alert(suma())
+    } else if (operacion == 'resta' || operacion == '-') {
+        alert(resta());
+    } else if (operacion == 'dividir' || operacion == 'divicion' || operacion == '/') {
+        alert(dividir());
+    } else if (operacion == 'multiplicar' || operacion == '*') {
+        alert(multiplicar());
+    }
 }
 
