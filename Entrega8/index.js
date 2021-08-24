4aa49518b37916eb4f5c697469b148ae472e4aaf
FernandoTol <60888678+FernandoTol@users.noreply.github.com>
2021-08-23 23:02:39 -0500

    entrega 9

--- Entrega8/index.js
+++ Entrega8/index.js
@@ -1,9 +1,22 @@
-const librosGuardados = [];
+function libreria(name, autor, editorial, category) {
+    this.name = name;
+    this.autor = autor;
+    this.editorial = editorial;
+    this.category = category;
+};
 
-while (librosGuardados != "no") {
-    let entrada = prompt("Ingrese sus libros");
-    librosGuardados.push(entrada.toUpperCase());
-    console.log(librosGuardados.length);
 
-    alert("Productos: " + librosGuardados);
+let saludo = prompt("Desea agregar libros? si / no");
+
+
+while (saludo != "no") {
+    let inputName = prompt("ingrese el nombre del libro");
+    let inputAutor = prompt("ingrese el autor");
+    let inputEditorial = prompt("ingrese la editorial");
+    let InputCategory = prompt("ingrese la categoria");
+    const persona1 = new libreria(inputName, inputAutor, inputEditorial, InputCategory);
+
+    alert(`usted lleva los libros de: ${persona1["name"]}`);
+
+    saludo = prompt('Desea agregar libros? si/no')
 }
